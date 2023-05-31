331c45d6d0e856bd3539cfe4885b007417777422
User <samsunga7sm7000@gmail.com>
2023-05-31 23:33:59 +0500

    Added ConnectionModule.kt

---
+++ modules/ui/src/main/java/com/example/ui/features/category/CategoriesFragment.kt
@@ -0,0 +1,11 @@
+package com.example.ui.features.category
+
+import androidx.fragment.app.Fragment
+
+// categories пакет для пример,так как техническом задание нет,
+// в актуальном проекте может быть по другому; Например в Каттабазаре
+
+class CategoriesFragment : Fragment() {
+
+
+}
